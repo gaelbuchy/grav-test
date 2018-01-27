6d3a76b3e07029210caf7867ceb5a921df6052b6
Gael <gaelfr67@gmail.com>
2018-01-27 17:12:42 -0600

    update responsive homepage

--- user/themes/nowheretheme/js/custom.js
+++ user/themes/nowheretheme/js/custom.js
@@ -3,4 +3,10 @@ $(document).ready(function (){
 
     $('.main-content-delayed').fadeIn(1000);
 
+    if($('.nav-collapse').length) {
+        var nav = responsiveNav(".nav-collapse", {
+            customToggle: ".nav-toggle"
+        });
+    }
+
 });
