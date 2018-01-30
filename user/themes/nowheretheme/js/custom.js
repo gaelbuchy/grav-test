a2ea149682097f8a74bdb1b66dad4057331f4a21
Gael <gaelfr67@gmail.com>
2018-01-30 17:56:09 -0600

    fix css bug

--- user/themes/nowheretheme/js/custom.js
+++ user/themes/nowheretheme/js/custom.js
@@ -1,12 +1,13 @@
 
 $(document).ready(function (){
 
-    $('.main-content-delayed').fadeIn(1000);
+    if($('.main-content-delayed').length) {
+        $('.main-content-delayed').fadeIn(1000);
+    }
 
     if($('.nav-collapse').length) {
         var nav = responsiveNav(".nav-collapse", {
             customToggle: ".nav-toggle"
         });
     }
-
 });
